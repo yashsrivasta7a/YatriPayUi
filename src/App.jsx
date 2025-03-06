232f84c1e9768e90f629c6d49d680c26c237375d
Yash Srivastava <ysyashmks@gmail.com>
2025-03-06 16:31:54 +0530

    Restore Home component as the root route; remove unused OB2-OB6 imports

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,8 @@ import Home from "./Pages/Home";
 import Signup from "./Pages/Signup";
 import Signin from "./Pages/Signin";
 import ForgotPassword from "./Pages/ForgotPassword";
-import OB2 from "./Components/OB2";
-import OB3 from "./Components/OB3";
-import OB4 from "./Components/OB4";
 import GlowEffect from "./Components/GlowEffect";
-import OB5 from "./Components/OB5";
-import OB6 from "./Components/OB6";
+
 import Onboarding from "./Pages/Onboarding";
 
 function App() {
@@ -18,7 +14,7 @@ function App() {
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<OB2 />} />
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
