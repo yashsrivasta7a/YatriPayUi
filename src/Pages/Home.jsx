0504b62b7aadc9dc09663869e5bade14deadf7fb
Yash Srivastava <ysyashmks@gmail.com>
2025-03-06 12:58:19 +0530

    Fix import path for YatriPay logo in Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import "../index.css";
 import { useNavigate } from "react-router-dom";
 import calendar from "../assets/calendar.svg";
-import logo from "../assets/yatripay_logo.svg"
+import logo from "../assets/yatripay_logo.svg"; // Fix the import path
+
 function Home() {
   const navigate = useNavigate();
   return (
-      <div className="content">
-        <div className="logo">
+    <div className="content">
+      <div className="logo">
         <img src={logo} alt="YatriPay" />
       </div>
-        <div className="home-text">
-          <h1>365 Days</h1>
-          <h2>Withdrawal</h2>
-        </div>
-        <img src={calendar} alt="calendarLogo" className="calendar" />
-        <button className="btn" onClick={() => navigate("/signup")}>
-          Get Started
-        </button>
+      <div className="home-text">
+        <h1>365 Days</h1>
+        <h2>Withdrawal</h2>
       </div>
-
+      <img src={calendar} alt="calendarLogo" className="calendar" />
+      <button className="btn" onClick={() => navigate("/signup")}>
+        Get Started
+      </button>
+    </div>
   );
 }
 
