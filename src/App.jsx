9278b59e39efa1c8a5ae23c50eeab38b1ccaef68
Yash Srivastava <ysyashmks@gmail.com>
2025-03-06 13:53:29 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
 import React from "react";
 import "./index.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./Pages/Home";
 import Signup from "./Pages/Signup";
-import GlowEffect from "./Components/GlowEffect";
 import Signin from "./Pages/Signin";
 import ForgotPassword from "./Pages/ForgotPassword";
 import OB2 from "./Pages/OB2";
 import OB3 from "./Pages/OB3";
 import OB4 from "./Pages/OB4";
+import GlowEffect from "./Components/GlowEffect";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-          <div>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="*" element={<Home />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/signin" element={<Signin />} />
-              <Route path="/password" element={<ForgotPassword />} />
-              <Route path="/ob2" element={<OB2 />} />
-              <Route path="/ob3" element={<OB3 />} />
-              <Route path="/ob4" element={<OB4 />} />
-            </Routes>
-          </div>
-
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/password" element={<ForgotPassword />} />
+          <Route path="/ob2" element={<OB2 />} />
+          <Route path="/ob3" element={<OB3 />} />
+          <Route path="/ob4" element={<OB4 />} />
+        </Routes>
+      </div>
       <div className="glow-background">
         <GlowEffect />
       </div>
-
-      </BrowserRouter>
-    </>
+    </Router>
   );
 }
 
